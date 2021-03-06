ef1a7f79cf66a6296d2ad5000e9174f4a120e6de
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-06 18:13:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,7 @@ if int % 3 == 0 #if the number int is diviisble by 3
   "Fizz" #Go fizz
 elsif int % 5 == 0 #if the number int is diviisble by 5
     "Buzz" #Go Buzz
-elsif int % 3 == 0 and 5 == 0 # if the number int is divisble by 3 and 5
+elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisble by 3 and 5
     "FizzBuzz" #Go FizzBuzz
   end
 end
