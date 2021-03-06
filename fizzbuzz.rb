be7c2e45741d7e544bfb59e513772fd324b30e41
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-06 17:58:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz (int)
 if int % 3 == 0 #if the number int is diviisble by 3
   "Fizz" #Go fizz
-if int % 5 == 0 #if the number int is diviisble by 5
+elsif condition
+   int % 5 == 0 #if the number int is diviisble by 5
     "Buzz" #Go Buzz
-if int % 3 == 0 and 5 == 0 # if the number int is divisble by 3 and 5
+elsif condition
+  int % 3 == 0 and 5 == 0 # if the number int is divisble by 3 and 5
     "FizzBuzz" #Go FizzBuzz
   end
 end
