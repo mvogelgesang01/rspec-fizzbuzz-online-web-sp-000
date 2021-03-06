111c087889447939d15337d236e7d1106ec77198
Melissa Vogelgesang <mvogelgesang01@gmail.com>
2021-03-06 17:31:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,3 @@ if int % 3 == 0 #if the number int is diviisble by 3
   "Fizz" #Go fizz
   end
 end
-
-fizzbuzz(3) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of nil
-fizzbuzz()  # => You should get an ArgumentError
